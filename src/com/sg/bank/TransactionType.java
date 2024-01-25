4c55c7ed5a5ad76dc4a60b4988ebfd2a2b64cca0
mohamedmakdoudi <makdoudi@gmail.com>
2024-01-25 11:37:29 +0100

    initial kata

---
+++ src/com/sg/bank/TransactionType.java
@@ -0,0 +1,9 @@
+package com.sg.bank;
+
+/**
+ * Enumération représentant les types de transactions possibles sur un compte bancaire.
+ */
+public enum TransactionType {
+    DEPOSIT, // Dépôt d'argent sur le compte
+    WITHDRAWAL // Retrait d'argent du compte
+}
